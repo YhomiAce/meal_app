7492ffe1e23ccaff7ff80201c270cbdfcae8bf96
YhomiAce <kareem.yomi@scelloo.com>
2022-01-03 22:51:00 +0100

    shop_app starter

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Delicacy"),
+      ),
+      body: Center(
+        child: Text("Navigation Time"),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
